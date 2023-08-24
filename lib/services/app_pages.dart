16676d06e09d4d4935651a8c88446c3aad48da45
hojune lim <lhj@infose.net>
2023-08-24 16:56:09 +0900

    .

--- lib/services/app_pages.dart
+++ lib/services/app_pages.dart
@@ -1,11 +1,12 @@
 import 'package:get/get.dart';
-import '../pages/home/index.dart' as home;
+import '/pages/home/index.dart' as home;
 
 abstract class AppPages {
   static final routes = [
     GetPage(
       name: '/home',
       page: () => home.Index(),
+      binding: home.Binding(),
     ),
   ];
 }
